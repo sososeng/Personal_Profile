3ae3ef633776c9b7a1c329cc7d349b1bf05ee4e7
Sokhun Seng <sososeng@gmail.com>
2017-05-31 21:09:55 -0500

    add more contents

--- style.js
+++ style.js
@@ -1,9 +1,11 @@
 (function ($) {
 
   $(document).foundation();
+
   $(document).ready(function(){
     
-  var lastScrollTop = 0;
+  /*var lastScrollTop = 0;
+
   $(window).scroll(function(event){
      var st = $(this).scrollTop();
      if (st > lastScrollTop){
@@ -12,12 +14,52 @@
         $('.top-bar').slideDown();
      }
      lastScrollTop = st;
-  });
+  });*/
 
+
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.top-bar').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .MagicMenu-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('.top-bar').slideUp();
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('.top-bar').slideDown();
+        }
+    }
+    
+    lastScrollTop = st;
+}
 
 
 });
 }(jQuery));
+
+
 
 
 //$(document).foundation();
